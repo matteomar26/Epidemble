2d16b06f34d895e3d34bdca0b0867a918533cf1f
MatteoMariani <matteomar.uni@gmail.com>
2022-07-25 14:28:21 +0000

    Code: added function for calculation of nu and used in the notebook

---
+++ functions.jl
@@ -0,0 +1,70 @@
+function calculate_ν!(ν,μ,neighbours,j,xi0,T)
+    if xi0 == 0
+        for τi = 0:T+1
+            for ti = 0:T+1
+                #first we check consistency between
+                # the planted time τi and the inferred 
+                #time ti by checking the observation constraint            
+                if ((ti==T+1)&&(τi<=T)) || ((ti<=T)&&(τi==T+1))  #if the observation is NOT satisfied
+                    continue  # ν = 0
+                end
+                #Since they both depend on ti only,
+                # we precaclulate the prior seed probability
+                # of the individual and the value of phi function 
+                # which is 1 if 0<ti<T+1 and 0 if ti=0,T+1
+                seed = (ti==0 ? γ : (1-γ) )
+                phi = (ti==0 || ti==T+1) ? 0 : 1
+                #now we calculate the four products over
+                # μ functions that we need to put in the
+                # expression of ν. We call them m1,..,m4
+                m1, m2, m3, m4 = ones(4)
+                # we initialize the m's to one and then we 
+                # loop a product over neighbours
+                for k in neighbours 
+                    m1 *= sum(μ[k,ti,1,τi,1:2])
+                    m2 *= sum(μ[k,ti,0,τi,1:2])
+                    m3 *= μ[k,ti,1,τi,2]
+                    m4 *= μ[k,ti,0,τi,2]
+                end
+                #Now we have everything to calculate ν
+                for tj=0:T+1                
+                    ν[ti,tj,τi,1] = seed * (a[ti-tj-1] * m1 - phi * a[ti-tj] * m2)
+                    # We use the fact that ν for σ=2 is just ν at σ=1 plus a term
+                    ν[ti,tj,τi,2] = ν[ti,tj,τi,1] + seed * (phi * a[ti-tj] * m4 - a[ti-tj-1] * m3)
+                end
+            end
+        end
+    else
+        # We are now in the case in which the individual is 
+        # the zero patient. In this case the computation of 
+        # the ν function is a little bit different than before
+        # so we separated the cases
+
+        for τi = 0:T+1
+            for ti = 0:T+1
+                if ((ti==T+1)&&(τi<=T)) || ((ti<=T)&&(τi==T+1))  #if the observation is NOT satisfied
+                    continue
+                end
+                #we can calculate ν now because it is constant
+                # in σ and is nonzero only if tj=0
+
+                #As before we pre-calculate ti-dependent quantities 
+                seed = (ti==0 ? γ : (1-γ) )
+                phi = (ti==0 || ti==T+1) ? 0 : 1
+                # We perform the product over neighbours
+                m1, m2 = ones(2)
+                for k in neighbours                
+                    m1 *= sum(μ[k,ti,1,τi,:])
+                    m2 *= sum(μ[k,ti,0,τi,:])
+                end
+                #We calculate ν in the zero patient case
+                ν[ti,0,τi,:] .= seed * (a[ti-1] * m1 - phi * a[ti] * m2)
+            end
+        end
+    end
+    if sum(ν) == 0
+        println("sum-zero ν")
+        return
+    end
+    ν ./= sum(ν);    
+end
