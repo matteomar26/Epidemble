508f9eb31266ff81f6d6f04d573d3b917e44821d
matteomar26 <matteomar.uni@gmail.com>
2022-11-08 09:21:49 +0000

    Discrete time BP

--- src/BP.jl
+++ src/BP.jl
@@ -1,15 +1,14 @@
 using PyCall
 
 @pyimport sib
-function sibyl(N, T_cont, Λ, O, γ, λ ; dt=1, maxit = 400, tol = 1e-14)
+function sibyl(N, T, Λ, O, γ, λ ; maxit = 400, tol = 1e-14)
     
-    T = Int(round(T_cont / dt))
     contacts = [(i-1,j-1,t, λ * dt) for t in 1:T for (i,j,v) in zip(findnz(Λ.A)...)];
     obs = [[(i,-1,t) for t=1:T for i=0:N-1];
-           [(i-1,s,Int(round(t/dt))) for (i,s,t,p) in O]]
+           [(i-1,s,t) for (i,s,t,p) in O]]
     sort!(obs, lt=((i1,s1,t1),(i2,s2,t2))->(t1<t2))
     prob_sus = 0.5
-    prob_seed=γ
+    prob_seed = γ
     pseed = prob_seed / (2 - prob_seed)
     psus = prob_sus * (1 - pseed)
     params = sib.Params(prob_r=sib.Exponential(mu=0), pseed=pseed, psus=psus,pautoinf=1e-10,fp_rate=1e-10,fn_rate=1e-10)
