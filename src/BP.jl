9dd1a28a90ff926e3daaa5d39ce0d5862955308b
matteomar26 <matteomar.uni@gmail.com>
2022-12-06 14:24:57 +0000

    false rate in sibyl

--- src/BP.jl
+++ src/BP.jl
@@ -2,7 +2,10 @@ using PyCall
 
 @pyimport sib
 function sibyl(N, T, Λ, O, γ, λ ; maxit = 400, tol = 1e-14)
-    
+    fr = 0
+    if length(O)>0
+        fr = O[1][4]
+    end
     contacts = [(i-1,j-1,t, λ) for t in 1:T for (i,j,v) in zip(findnz(Λ.A)...)];
     obs = [[(i,-1,t) for t=1:T for i=0:N-1];
            [(i-1,s,t) for (i,s,t,p) in O]]
@@ -11,7 +14,7 @@ function sibyl(N, T, Λ, O, γ, λ ; maxit = 400, tol = 1e-14)
     prob_seed = γ
     pseed = prob_seed / (2 - prob_seed)
     psus = prob_sus * (1 - pseed)
-    params = sib.Params(prob_r=sib.Exponential(mu=0), pseed=pseed, psus=psus,pautoinf=1e-10,fp_rate=1e-10,fn_rate=1e-10)
+    params = sib.Params(prob_r=sib.Exponential(mu=0), pseed=pseed, psus=psus,pautoinf=1e-10,fp_rate=fr,fn_rate=fr)
     f = sib.FactorGraph(contacts=contacts, observations=obs, params=params)
     sib.iterate(f, maxit=maxit,tol=tol)
     sib.iterate(f, maxit=maxit, damping=0.5, tol=tol)
