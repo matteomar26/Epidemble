888489bc5534ebe540cbdb92ed06cd56b8575edf
matteomar26 <matteomar.uni@gmail.com>
2022-12-22 09:45:33 +0000

    single instance code aggregated to same file

--- src/single_instance.jl
+++ src/single_instance.jl
@@ -26,3 +26,51 @@ function sibyl(N, T, Λ, O, γ, λ ; maxit = 400, tol = 1e-14)
     end 
     return m_sib
 end
+
+function tpr(xtrue, rank) 
+    cumsum(xtrue[rank]) ./( cumsum(xtrue[rank])[end])
+end
+
+
+function fpr(xtrue, rank) 
+    N = size(rank,1)
+    return (range(1,N,length=N) .- cumsum(xtrue[rank])) ./ (range(1,N,length=N) .- cumsum(xtrue[rank]) )[end]
+end
+
+
+function ROC(xtrue, p)
+    N = size(xtrue,1)
+    rank = sortperm(p, rev=true)
+    return fpr(xtrue, rank) , tpr(xtrue, rank)
+end
+
+    
+function AUROC(ROC)
+    N = size(ROC[1],1) 
+    AU = 0
+    for t = 1:N-1
+        AU += ROC[2][t] * (ROC[1][t+1] - ROC[1][t])
+    end
+    return AU
+end
+
+function sample!(x, G, λi, γi) 
+    N, T = size(x)
+    for i=1:N
+        x[i,1] = rand() < γi
+    end
+    for t = 2:T
+        for i = 1:N
+            if x[i,t-1] == 1
+                x[i,t] = 1
+                continue
+            end
+            r = 1
+            for j in inedges(G,i)
+                r *= 1 - λi * x[j.src,t-1] 
+            end
+            x[i,t] = rand() > r
+        end
+    end
+    x
+end
