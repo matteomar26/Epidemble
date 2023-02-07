43d9f36034d4ffb6857ab8983399f138f6d7a075
matteomar26 <matteomar.uni@gmail.com>
2023-02-07 13:21:31 +0000

    src code reshaped. Only pop-dynamics functions in functions.jl

--- src/functions.jl
+++ src/functions.jl
@@ -230,8 +230,6 @@ function rand_disorder(γp, λp, dist)
     return xi0, sij, sji, d
 end
 
-
-
 function pop_dynamics(N, T, λp, λi, γp, γi, degree_dist; tot_iterations = 5, fr = 0.0, dilution = 0.0)
     μ = fill(1.0 / (6*(T+2)^2), 1:N, 0:T+1, 0:1, 0:T+1, 0:2)
     Paux = fill(0.0, 0:1, 0:2)
@@ -282,127 +280,6 @@ function pop_dynamics(N, T, λp, λi, γp, γi, degree_dist; tot_iterations = 5,
     return marg
 end
 
-
-function PhaseDiagram(γvalues, λvalues, N, T, dist, paramdist; tot_iterations=2, fr = 0.0, dilution = 0.0)
-    inf_out = zeros(length(γvalues),length(λvalues), T + 2) # 1 value for pdiag and T+1 values for the AUC
-    pr = Progress(length(γvalues) * length(λvalues))
-    Threads.@threads for (γcount,λcount) in collect(product(1:length(γvalues),1:length(λvalues)))
-        λi = λp = λvalues[λcount]
-        γi = γp = γvalues[γcount]
-        marg = pop_dynamics(N, T, λp, λi, γp, γi, dist, paramdist, tot_iterations = tot_iterations, fr=fr, dilution=dilution)
-        marg2D = reshape((sum(marg,dims=1)./ N),T+2,T+2);
-        # we sum over the trace of the 2D marginal to find the probability to infere correctly
-        inf_out[γcount,λcount,1] = diag(marg2D)
-        inf_out[γcount,λcount,2:end] .= avgAUC(marg)
-        ProgressMeter.next!(pr)
-    end
-    return inf_out
-end
-
-
-function diag(marg2D)
-    T = size(marg2D,1) - 2
-    sum([marg2D[t,t] for t=1:T+2])
-end
-
-function L1(marg2D) 
-    T = size(marg2D,1) - 2
-    [sum(marg2D[1:t,1:t]) + sum(marg2D[t+1:end,t+1:end]) for t=1:T+1]
-end
-
-function L1(marg2D) 
-    T = size(marg2D,1) - 2
-    [sum(marg2D[1:t,1:t]) + sum(marg2D[t+1:end,t+1:end]) for t=1:T+1]
-end
-
-
-function L1bis(marg)
-    N = size(marg,1)
-    T = size(marg,2) - 2
-    p_agree = OffsetArrays.OffsetArray(zeros(T+1),-1)
-    for t = 0 : T 
-        for l = 1 : N
-            for τi = 0 : T + 1
-                (sum(marg[l, :, τi]) == 0) && continue
-                pi_inf = sum(marg[l,0:t,τi]) 
-                p_agree[t] += (τi <= t ? pi_inf : 1 - pi_inf) #is the planted infected? if so take the prob of being infected 
-            end
-        end
-    end
-    p_agree ./= N
-    return [p_agree[t] for t = 0:T]
-end
-
-
-
-function MSE(marg)
-    N = size(marg,1)
-    T = size(marg,2) - 2
-    sq_err = OffsetArrays.OffsetArray(zeros(T+1),-1)
-    for t = 0 : T 
-        for l = 1 : N
-            for τi = 0 : T + 1 
-                (sum(marg[l, :, τi]) == 0) && continue
-                xi_pt = (τi <= t)
-                pi_inf = sum(marg[l,0:t,τi]) 
-                sq_err[t] += (pi_inf - xi_pt) ^ 2 
-            end
-        end
-    end
-    sq_err ./= N
-    return [sq_err[t] for t = 0:T]
-end
-
-
-
-function avgOverlap(marg)
-    N = size(marg,1)
-    T = size(marg,2) - 2
-    overlap = OffsetArrays.OffsetArray(zeros(T+1),-1)
-    for t = 0 : T 
-        for l = 1 : N
-            for τi = 0 : T + 1
-                (sum(marg[l, :, τi]) == 0) && continue
-                xi_pt = (τi <= t)
-                xi_inf = sum(marg[l,0:t,τi]) > 0.5
-                overlap[t] += (xi_pt == xi_inf)
-            end
-        end
-    end
-    overlap ./= N
-    return [overlap[t] for t = 0:T]
-end
-
-function avgAUC(marg)
-    N = size(marg,1)
-    T = size(marg,2) - 2
-    AUC = OffsetArrays.OffsetArray(zeros(T+1),-1)
-    count = OffsetArrays.OffsetArray(zeros(T+1),-1)
-    for l = 1 :  N 
-        for m = l + 1 : min(N,l+400)
-            result = 0
-            for τi = 0 : T 
-                (sum(marg[l, :, τi]) == 0 ) && continue
-                for τj = τi + 1 : T + 1
-                    (sum(marg[m, :, τj]) == 0) && continue
-                    # at the perfect inference for t=τj you would sum the diagonal
-                    for t = τi : τj - 1
-                        count[t] += 1
-                        pi = sum(marg[l, 0:t, τi])
-                        pj = sum(marg[m, 0:t, τj])
-                        if pi ≈ pj
-                            AUC[t] += 1/2
-                        elseif pi > pj
-                            AUC[t] += 1
-                        end
-                    end
-                end
-            end
-        end
-    end
-    AUC ./= count
-    return [AUC[t] for t = 0:T]
-end
 
 function makeDistrib(degreetype,d)
     if degreetype == "poisson"
