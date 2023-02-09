b48373a8513e00a39ae1b23354e0d8676c2ceaab
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-02-09 15:36:00 +0100

    bug fix: cavity planted times

--- src/update.jl
+++ src/update.jl
@@ -38,32 +38,33 @@ end
 
 function update!(M::Model, i)
     @unpack fr, γi, T, Λ, μ, belief, τμ, τbelief = M
-    xi0,∂i,S,oi = rand_disorder(M)
-    τi = xi0 ? 0 : minimum(τ+s for (τ,s) in zip(view(τμ,∂i), S); init=T+1)
+    xi0,∂in,S,oi = rand_disorder(M)
+    τout, τi = cavity(τμ[∂in] .+ S, min, xi0 ? 0 : T+1)
     τbelief[i] = τi
-    ∂out = rand(1:popsize(M), length(∂i))
-    μ1, μ0 = μ[:,1,∂i], μ[:,0,∂i]
+    ∂out = rand(1:popsize(M), length(∂in))
+    μ1, μ0 = μ[:,1,∂in], μ[:,0,∂in]
     μ[:, :, ∂out] .= 0
     belief[:, i] .= 0
-    τμ[∂out] .= τi
-    M1,M0 = zeros(length(∂i)), zeros(length(∂i))
+    τμ[∂out] .= τout
+    M1,M0 = zeros(length(∂in)), zeros(length(∂in))
     for ti = 0:T+1
         ξ = obs(M, ti, τi, oi)
         iszero(ξ) && continue
-        seed = iszero(ti) ? γi : 1 - γi
+        pseed = iszero(ti) ? γi : 1 - γi
         phi = 0 < ti < T+1
         m1full = cavity!(M1, (@view μ1[ti, :]), *, 1.0)
         m0full = cavity!(M0, (@view μ0[ti, :]), *, 1.0)
         for (j, m1, m0) in zip(∂out, M1, M0)
             for tj in 0:T+1
-                # νij[ti,tj]              
-                ν = ξ * seed * (Λ[ti - tj - 1] * m1 - phi * Λ[ti - tj] * m0)
-                #ν = ξ * ((ti == 0 ? 1.0 : 1-γi) * a[ti-tj-1] * m1 - (1-γi) * a[ti-tj] * m0)
+                # νij[ti,tj]
+                #@assert m1 == m0 == 1
+                ν = ξ * pseed * (Λ[ti - tj - 1] * m1 - phi * Λ[ti - tj] * m0)
+                #ν = ξ * ((ti == 0 ? 1.0 : (1 - γi)) * Λ[ti - tj - 1] * m1 - (1 - γi) * Λ[ti - tj] * m0)
                 μ[tj, 1, j] += ν * Λ[tj - ti - 1]
                 μ[tj, 0, j] += ν * Λ[tj - ti]
             end
         end
-        belief[ti, i] = ξ * seed * (m1full - phi * m0full)
+        belief[ti, i] = ξ * pseed * (m1full - phi * m0full)
     end
     normalize!(@view belief[:, i])
     for j in ∂out
@@ -75,15 +76,16 @@ residual(d::Poisson) = d #residual degree of poiss distribution is poisson with 
 residual(d::Dirac) = Dirac(d.value - 1) #residual degree of rr distribution (delta) is a delta at previous vale
 residual(d::DiscreteNonParametric) = DiscreteNonParametric(support(d) .- 1, (probs(d) .* support(d)) / sum(probs(d) .* support(d)))
 
+geometric(r) = floor(Int, log(rand())*r) + 1
+
 function rand_disorder(M::Model)
     r = 1 / log(1 - M.λp)
-    delay(r) = floor(Int, log(rand())*r) + 1
     xi0 = rand() < M.γp;
     d = rand(M.distribution)
-    S = [delay(r) for _ = 1:d]
+    S = [geometric(r) for _ in 1:d]
     oi = rand() > M.dilution
-    ∂i = rand(1:popsize(M), d)
-    return xi0, ∂i, S, oi
+    ∂in = rand(1:popsize(M), d)
+    (;xi0, ∂in, S, oi)
 end
 
 
