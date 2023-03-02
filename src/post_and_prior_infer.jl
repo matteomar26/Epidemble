a463d215256f2a316b3c17016f90fa8059b33ac0
matteomar26 <matteomar.uni@gmail.com>
2023-03-02 14:07:25 +0000

    derivative of messages implemented

--- src/post_and_prior_infer.jl
+++ src/post_and_prior_infer.jl
@@ -144,49 +144,59 @@ function compute_∂zψi(M::ParametricModel,l,neighbours,xi0,oi)
 end
 
 function compute_∂zψij(M::ParametricModel,neighbours,xi0,oi)
-    @unpack T,γi,Λ,μ,∂μ = M
+    @unpack T,γi,Λ,μ,∂μ,∂ν = M
     ∂zψij = 0.0
     if xi0 == 0
         for τi = 1:T+1
             for ti = 0:T+1
-                #first we check consistency between
-                # the planted time τi and the inferred 
-                #time ti by checking the observation constraint
                 ξ = obs(M,ti,τi,oi)
                 if ξ == 0.0 #if the observation is NOT satisfied
                     continue  # ν = 0
                 end
-                #Since they both depend on ti only,
-                # we precaclulate the prior seed probability
-                # of the individual and the value of phi function 
-                # which is 1 if 0<ti<T+1 and 0 if ti=0,T+1
                 seed = (ti==0 ? γi : (1-γi) )
                 phi = (ti==0 || ti==T+1) ? 0 : 1
-                #now we calculate the four products over
-                # μ functions that we need to put in the
-                # expression of ν. We call them m1,..,m4
+                #first part of Leibniz rule: derivative of lambda
                 m1, m2, m3, m4 = 1.0, 1.0, 1.0, 1.0
-                # we initialize the m's to one and then we 
-                # loop a product over neighbours
                 for k in neighbours 
                     m1 *= μ[ti,1,τi,1,k] + μ[ti,1,τi,2,k]
                     m2 *= μ[ti,0,τi,1,k] + μ[ti,0,τi,2,k]
                     m3 *= μ[ti,1,τi,2,k]
                     m4 *= μ[ti,0,τi,2,k]
                 end
-                #Now we have everything to calculate ν
                 for tj=0:T+1                
-                    ν[ti,tj,τi,1] = ξ  * seed * (Λ[ti-tj-1] * m1 - phi * Λ[ti-tj] * m2)
+                    ∂ν[ti,tj,τi,1] = ξ  * seed * (∂Λ[ti-tj-1] * m1 - phi * ∂Λ[ti-tj] * m2)
                     # We use the fact that ν for σ=2 is just ν at σ=1 plus a term
-                    ν[ti,tj,τi,2] = ν[ti,tj,τi,1] + ξ * (τi<T+1) * seed * (phi * Λ[ti-tj] * m4 - Λ[ti-tj-1] * m3)
+                    ∂ν[ti,tj,τi,2] = ∂ν[ti,tj,τi,1] + ξ * (τi<T+1) * seed * (phi * ∂Λ[ti-tj] * m4 - ∂Λ[ti-tj-1] * m3)
+                end
+                #second part: derivative of the message
+                m1, m2, m3, m4 = 0.0, 0.0, 0.0, 0.0
+                for j in neighbours
+                    m1∂ = ∂μ[ti,1,τi,1,j] + ∂μ[ti,1,τi,2,j]
+                    m2∂ = ∂μ[ti,0,τi,1,j] + ∂μ[ti,0,τi,2,j]
+                    m3∂ = ∂μ[ti,1,τi,2,j]
+                    m4∂ = ∂μ[ti,0,τi,2,j]
+                    for k in neighbours
+                        (k == j) && (continue)
+                        m1∂ *= μ[ti,1,τi,1,k] + μ[ti,1,τi,2,k]
+                        m2∂ *= μ[ti,0,τi,1,k] + μ[ti,0,τi,2,k]
+                        m3∂ *= μ[ti,1,τi,2,k]
+                        m4∂ *= μ[ti,0,τi,2,k] 
+                    end
+                    m1 += m1∂
+                    m2 += m2∂
+                    m3 += m3∂
+                    m4 += m4∂
+                end
+                for tj=0:T+1
+                    tmp = ξ  * seed * (Λ[ti-tj-1] * m1 - phi * Λ[ti-tj] * m2)
+                    ∂ν[ti,tj,τi,1] += tmp
+                    # We use the fact that ν for σ=2 is just ν at σ=1 plus a term
+                    ∂ν[ti,tj,τi,2] += tmp + ξ * (τi<T+1) * seed * (phi * Λ[ti-tj] * m4 - Λ[ti-tj-1] * m3)
                 end
             end
         end
     else
-        # We are now in the case in which the individual is 
-        # the zero patient. In this case the computation of 
-        # the ν function is a little bit different than before
-        # so we separated the cases
+        #zero patient case
 
         for tj = 0:T+1
             for ti = 0:T+1
@@ -194,20 +204,30 @@ function compute_∂zψij(M::ParametricModel,neighbours,xi0,oi)
                 if ξ == 0.0  #if the observation is NOT satisfied
                     continue
                 end
-                #we can calculate ν now because it is constant
-                # in σ and is nonzero only if τi=0
-
-                #As before we pre-calculate ti-dependent quantities 
+                
                 seed = (ti==0 ? γi : (1-γi) )
                 phi = (ti==0 || ti==T+1) ? 0 : 1
-                # We perform the product over neighbours
+                
                 m1, m2 = 1.0, 1.0
                 for k in neighbours                
                     m1 *= μ[ti,1,0,0,k] + μ[ti,1,0,1,k] + μ[ti,1,0,2,k]
                     m2 *= μ[ti,0,0,0,k] + μ[ti,0,0,1,k] + μ[ti,0,0,2,k]
                 end
-                #We calculate ν in the zero patient case
-                ν[ti,tj,0,:] .= ξ * seed * (Λ[ti-1-tj] * m1 - phi * Λ[ti-tj] * m2)
+                #First part of Leibniz rule
+                ∂ν[ti,tj,0,:] .= ξ * seed * (∂Λ[ti-1-tj] * m1 - phi * ∂Λ[ti-tj] * m2)
+                m1, m2 = 0.0, 0.0
+                for j in neighbours
+                    m1∂ = ∂μ[ti,1,0,0,j] + ∂μ[ti,1,0,1,j] + ∂μ[ti,1,0,2,j]
+                    m2∂ = ∂μ[ti,0,0,0,j] + ∂μ[ti,0,0,1,j] + ∂μ[ti,0,0,2,j]
+                    for k in neighbours
+                        (k==j) && (continue)
+                        m1∂ *= μ[ti,1,0,0,k] + μ[ti,1,0,1,k] + μ[ti,1,0,2,k]
+                        m2∂ *= μ[ti,0,0,0,k] + μ[ti,0,0,1,k] + μ[ti,0,0,2,k]
+                    end
+                    m1 += m1∂
+                    m2 += m2∂
+                end
+                ∂ν[ti,tj,0,:] .+= ξ * seed * (Λ[ti-1-tj] * m1 - phi * Λ[ti-tj] * m2)
             end
         end
     end
