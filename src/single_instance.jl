4a019055da4d415053934c79c0f67b9fe96cfdfa
matteomar26 <matteomar.uni@gmail.com>
2023-04-21 13:55:42 +0000

    code for hyper inference

--- src/single_instance.jl
+++ src/single_instance.jl
@@ -14,8 +14,8 @@ function sibyl(N, T, Λ, O, γ, λ ; maxit = 400, tol = 1e-14, learn=false)
     params = sib.Params(prob_r=sib.Exponential(mu=0), pseed=pseed, psus=psus,pautoinf=1e-10)
     f = sib.FactorGraph(contacts=contacts, observations=obs, params=params)
     sib.iterate(f, maxit=maxit,tol=tol ; learn)
-    sib.iterate(f, maxit=maxit, damping=0.5, tol=tol ; learn)
-    sib.iterate(f, maxit=maxit, damping=0.9, tol=tol ; learn)
+    #sib.iterate(f, maxit=maxit, damping=0.5, tol=tol ; learn)
+    #sib.iterate(f, maxit=maxit, damping=0.9, tol=tol ; learn)
     p_sib=[collect(n.bt) for n in f.nodes]
     m_sib = zeros(N, T)
     dλ = 0.0
@@ -25,9 +25,47 @@ function sibyl(N, T, Λ, O, γ, λ ; maxit = 400, tol = 1e-14, learn=false)
         for t=2:T
             m_sib[i,t] = m_sib[i,t-1] + p_sib[i][t]
         end
-    end 
+    end
+    
     return m_sib, dλ/N
 end
+
+#=function sibyl(N, T, Λ, O, γ, λ ; iters = 40, tol = 1e-14, learn=false,eta_learn=0.03)
+    Infsteps = zeros(iters,2)
+    f = 0
+    obs = [[(i,-1,t) for t=1:T for i=0:N-1];
+           [(i-1,s,t) for (i,s,t,p) in O]]
+    sort!(obs, lt=((i1,s1,t1),(i2,s2,t2))->(t1<t2))
+    prob_sus = 0.5
+    m_sib = zeros(N, T)
+    for st = 1:iters
+        contacts = [(i-1,j-1,t, λ) for t in 1:T for (i,j,v) in zip(findnz(Λ.A)...)];
+        prob_seed = γ
+        pseed = prob_seed / (2 - prob_seed)
+        psus = prob_sus * (1 - pseed)
+        params = sib.Params(prob_r=sib.Exponential(mu=0), pseed=pseed, psus=psus,pautoinf=1e-10)
+        f = sib.FactorGraph(contacts=contacts, observations=obs, params=params)
+        sib.iterate(f, maxit=1,tol=1e-10 ; learn)
+        # if learn == flase && tolerance < tol ==> return m_sib,Infsteps. Come si trova tolerance??
+        p_sib=[collect(n.bt) for n in f.nodes]
+        if (learn == true && st>3)
+            dλ = sum(f.nodes[i].df_i[1] for i = 1:N, init=0.0) #accumulate derivative on lambda
+            λ += abs(λ) * eta_learn * sign(dλ) #update lambda
+            λ = clamp(λ,0.0,1.0) #clamp lambda
+            γ = sum(p_sib[i][1] for i = 1:N, init=0.0)/N #update gamma
+        end
+        Infsteps[st,1] = γ
+        Infsteps[st,2] = λ
+    end
+    p_sib = [collect(n.bt) for n in f.nodes]
+    for i=1:N
+        m_sib[i,1] = p_sib[i][1] 
+        for t=2:T
+            m_sib[i,t] = m_sib[i,t-1] + p_sib[i][t]
+        end
+    end    
+    return m_sib, Infsteps
+end=#
 
 function tpr(xtrue, rank) 
     cumsum(xtrue[rank]) ./( cumsum(xtrue[rank])[end])
